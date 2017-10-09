8ab7dce016383e54cfeaaa498f7be8fee375be2c
endrina rivas <endrinita@gmail.com>
2017-10-09 01:05:13 -0400

    changed web design

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-	return render(request, 'index.html', {})
+	return render(request, 'index2.html', {})
 
 def resume(request):
 	return render(request, 'resume.html', {})
